51e5002cf9428b392d920236758d87b628eb339b
virajm428 <virajm428@gmail.com>
2026-06-19 05:16:48 +0000

    Manually add tauri updater plugin to rust code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_updater::Builder::new().build()) // 🟢 මෙන්න මේ පේළිය අලුතින් එකතු කළා
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
